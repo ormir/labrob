2b599981f97f0741331d78285555e5a3141783ef
Ormir G <ormirgj@gmail.com>
2016-05-13 14:26:01 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  labrob
+//
+//  Created by Ormir G. on 13/05/16.
+//  Copyright © 2016 Nitika Kumar, Ormir Gjurgjej. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
