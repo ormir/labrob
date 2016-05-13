62ed5fe7449267d7f5ca610cb20456335a7170a3
NitikaK <dollysharma@dollys-MacBook-Air.local>
2016-05-13 17:22:14 +0200

    entry/exit

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -16,6 +16,7 @@ Labyrinth::Labyrinth(std::string filename) {
         std::string line;
         while(std::getline(file, line))  {
             //all vectors within lab vector
+            //if a line of the maze only contains one character, don't save in lab vector
             if (line.size() > 1) {
                 std::vector<char> labline(line.begin(), line.end());
                 //labline is "pushed into"(stored) in lab vector
@@ -24,8 +25,55 @@ Labyrinth::Labyrinth(std::string filename) {
         }
     }
     file.close();
+    
+    entry[0]= {-1};
+    exit [0] = {-1};
+    width = (int) lab.begin()->size();
+    height = (int) lab.size();
+    
+    //first vector -> first character until first vector -> last character
+    for(auto it = lab.begin()->begin(); it != lab.begin()->end(); ++it){
+        if((*it) == ' ' && entry[0] == -1) {
+            entry [0] = (int) (it - lab.begin()->begin());
+            entry [1] = 0;
+        } else if ((*it) == ' ' && exit[0] == -1) {
+            exit [0] = (int) (it - lab.begin()->begin());
+            exit [1] = 0;
+        }
+    }
+    for (auto it = lab.begin(); it != lab.end(); ++it){
+        if(*(it->end()) == ' ' && entry[0] == -1){
+            entry [0] = width-1;
+            entry [1] = (int) (it - lab.begin());
+        } else if (*(it->end()) == ' ' && exit[0] == -1) {
+            exit [0] = width-1;
+            exit [1] = (int) (it - lab.begin());
+        }
+    }
+    
+    for (auto it = lab.rbegin()->rbegin(); it != lab.rend()->rend(); ++it){
+        if((*it) == ' ' && entry[0] == -1){
+            entry [0] = (int) (it - lab.rbegin()->rend());
+            entry [1] = height-1;
+        } else if ((*it) == ' ' && exit[0] == -1){
+            exit [0] = (int) (it - lab.rbegin()->rend());
+            exit [1] = height-1;
+        }
+    }
+    
+    for (auto it = lab.rbegin(); it != lab.rend(); ++it){
+        if(*(it->begin()) == ' ' && entry[0] == -1){
+            entry[0] = 0;
+            entry[1] = (int) (it-lab.rend());
+        } else if (*(it->begin()) == ' ' && exit[0] == -1) {
+            exit[0] = 0;
+            exit[1] = (int) (it-lab.rend());
+        }
+    }
+        
 }
 
+           
 void Labyrinth::show() const{
     //don't use int i dammit, use iteratoooorsss. data types of iterator --> auto (short)
     for(auto it = lab.begin(); it != lab.end(); ++it){
@@ -34,6 +82,10 @@ void Labyrinth::show() const{
         }
         std::cout << std::endl;
     }
-}
+    std::cout << "entry: " << entry[0] << "," << entry[1] << std::endl;
+    std::cout << "exit: " << exit [0] << "," << exit[1] << std::endl;
+    std::cout << "height: " << height << std::endl;
+    std::cout << "width: " << width << std::endl;
+ }
 
 Labyrinth::~Labyrinth(){}
