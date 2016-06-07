48aeb64b898444956974a05068f9965354574d5c
Ormir G <ormirgj@gmail.com>
2016-06-07 13:43:18 +0200

    delete old thread

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 //
 
 #include <iostream>
-#include <thread>
 #include "labyrinth.hpp"
 #include "wallfollower.hpp"
 #include "tremaux.hpp"
@@ -28,7 +27,6 @@ int main(int argc, const char * argv[]) {
                 std::cout << "Robot finished on " << rob.getSteps() << " steps" << std::endl;
             } else {
                 std::vector<Robot*> robList;
-                std::vector<std::thread> threads;
                 
                 for (int i = 2; i < argc; i++) {
                     std::string arg = argv[i];
@@ -42,11 +40,8 @@ int main(int argc, const char * argv[]) {
                 
                 // Solve robots
                 for(auto it = robList.begin(); it != robList.end(); ++it)
-                    threads.push_back(std::thread((*it)->solve()));
-                
-                // Join threads
-                for (auto it = threads.begin(); it != threads.end(); ++it)
-                    it->join();
+                    (*it)->solve();
+            
                 
                 // Show steps
                 for(auto it = robList.begin(); it != robList.end(); ++it)
